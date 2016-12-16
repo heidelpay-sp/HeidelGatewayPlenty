75076cc78b153d225498de39c600f2c2de738229
Sascha Pflüger <sascha.pflueger@heidelpay.de>
2016-12-16 15:47:07 +0100

    Class renamed

--- src/Providers/HeidelGatewayPlentyServiceProvider.php
+++ src/Providers/HeidelGatewayPlentyServiceProvider.php
@@ -47,7 +47,7 @@ class HeidelGatewayPlentyServiceProvider extends ServiceProvider
 		// Register Creditcard payment method in the payment method container
 		$payContainer->register(
 				'HeidelGatewayPlenty::CREDITCARD', 
-				CreditcardPaymentMethod::class,
+				HeidelGatwayPlentyCreditcardPaymentMethod::class,
 				[ AfterBasketChanged::class, AfterBasketItemAdd::class, AfterBasketCreate::class ]
 				);
 
