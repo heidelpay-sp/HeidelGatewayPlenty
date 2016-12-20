1eda1a29b62df72c967877a8ca24f4707237f343
Sascha Pflüger <sascha.pflueger@heidelpay.de>
2016-12-20 17:26:02 +0100

    new route provider

---
+++ src/Providers/HeidelGatewayRouteServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace HeidelGatewayPlenty\Providers;
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+
+/**
+ * Class PayPalRouteServiceProvider
+ * @package PayPal\Providers
+ */
+class PayPalRouteServiceProvider extends RouteServiceProvider
+{
+	/**
+	 * @param Router $router
+	 */
+	public function map(Router $router)
+	{
+		// Get the PayPal success and cancellation URLs
+		$router->get('HeidelGatewayPlenty/checkoutSuccess'	, 'HeidelGatewayPlenty\Controllers\PaymentController@checkoutSuccess');
+		$router->get('HeidelGatewayPlenty/checkoutCancel' 	, 'HeidelGatewayPlenty\Controllers\PaymentController@checkoutCancel' );
+		$router->post('HeidelGatewayPlenty/notification'  	, 'HeidelGatewayPlenty\Controllers\PaymentNotificationController@handleNotification');
+	}
+
+
+
+}	
