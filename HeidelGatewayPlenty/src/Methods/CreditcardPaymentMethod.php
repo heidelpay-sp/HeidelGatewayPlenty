73e6320946b2c01f4235612bfbefd957cedc4abe
Sascha Pflüger <sascha.pflueger@heidelpay.de>
2017-01-09 17:40:43 +0100

    removed in getName() a piece of pluginkey

--- HeidelGatewayPlenty/src/Methods/CreditcardPaymentMethod.php
+++ HeidelGatewayPlenty/src/Methods/CreditcardPaymentMethod.php
@@ -65,7 +65,7 @@ class CreditcardPaymentMethod extends PaymentMethodService
 	 */
 	public function getName( ConfigRepository $configRepository )
 	{
-		$name = $configRepository->get('HeidelGatewayPlenty.basicData.hgwcc.name');
+		$name = $configRepository->get('basicData.hgwcc.name');
 		
 		if(!strlen($name))
 		{
