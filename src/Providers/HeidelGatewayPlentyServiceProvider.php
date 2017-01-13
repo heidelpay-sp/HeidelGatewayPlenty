85545523c7d25c0e4d5db46506dcfa576029c8aa
Sascha Pflüger <sascha.pflueger@heidelpay.de>
2017-01-13 15:27:05 +0100

    test commit

--- src/Providers/HeidelGatewayPlentyServiceProvider.php
+++ src/Providers/HeidelGatewayPlentyServiceProvider.php
@@ -76,7 +76,7 @@ class HeidelGatewayPlentyServiceProvider extends ServiceProvider
 						$warenkorb = $warenkorb->load();
 						
 						$channel = $configRepository->get('HeidelGatewayPlenty.hgw_ccChannel');
-						$event->setValue('<h1>Heidelpay GetPaymentMethodContent<h1>' .$channel.' hier Zahlartname');
+						$event->setValue('<h1>Heidelpay GetPaymentMethodContent<h1>' .$channel.' hier Channel');
 						$event->setType('htmlContent');
 					}
 		});
