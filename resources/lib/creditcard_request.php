f13397aca9ad9eb98a5b867bd0bc181f1dd4d0e1
Sascha Pflüger <sascha.pflueger@heidelpay.de>
2017-07-03 11:03:27 +0200

    variable umbenannt

--- resources/lib/creditcard_request.php
+++ resources/lib/creditcard_request.php
@@ -1,52 +1,55 @@
 <?php
 use Plenty\Plugin\ConfigRepository;
 
-        $configRepository = new ConfigRepository;
+    $configRepository = new ConfigRepository;
 
+    // Filling Request-Object with Data
+    $creditCardMethod = new \Heidelpay\PhpApi\PaymentMethods\CreditCardPaymentMethod();
+    $paramsToSend = array();
+    $paramsToSend[0] = $configRepository->get('HeidelGatewayPlenty.securitySender');
+    $paramsToSend[1] = $configRepository->get('HeidelGatewayPlenty.login');
+    $paramsToSend[2] = $configRepository->get('HeidelGatewayPlenty.password');
+    $paramsToSend[3] = $configRepository->get('HeidelGatewayPlenty.hgw_cc_channel');
+    $paramsToSend[4] = true;
 
-        $cardPaymentMethod = new \Heidelpay\PhpApi\PaymentMethods\CreditCardPaymentMethod();
-        $paramsToSend = array();
-        $paramsToSend[0] = $configRepository->get('HeidelGatewayPlenty.securitySender');
-        $paramsToSend[1] = $configRepository->get('HeidelGatewayPlenty.login');
-        $paramsToSend[2] = $configRepository->get('HeidelGatewayPlenty.password');
-        $paramsToSend[3] = $configRepository->get('HeidelGatewayPlenty.hgw_cc_channel');
-        $paramsToSend[4] = true;
-        if ($configRepository->get('HeidelGatewayPlenty.transactionmode')) {
-            $paramsToSend[4] = false;
-        }
+    if ($configRepository->get('HeidelGatewayPlenty.transactionmode')) {
+        $paramsToSend[4] = false;
+    }
 
-        $cardPaymentMethod->getRequest()->authentification($paramsToSend);
+    $creditCardMethod->getRequest()->authentification($paramsToSend);
 
-        $cardPaymentMethod->getRequest()->customerAddress(
-            'John',
-            'Doe',
-            null,
-            '12345',
-            'Vangerowstr. 5',
-            null,
-            '69115',
-            'Heidelberg',
-            'Deutschland',
-            'sascha.pflueger@heidelpay.de'
-        );
-        $cardPaymentMethod->getRequest()->basketData(
-            '1234',
-            '15.30',
-            'EUR',
-            $configRepository->get('HeidelGatewayPlenty.secret')
-        );
+    $creditCardMethod->getRequest()->customerAddress(
+       'John',
+       'Doe',
+       null,
+       '12345',
+       'Vangerowstr. 5',
+       null,
+       '69115',
+       'Heidelberg',
+       'Deutschland',
+       'sascha.pflueger@heidelpay.de'
+    );
 
-        $cardPaymentMethod->getRequest()->async(
-            'DE',
-            'https://heidelpay-dev.plentymarkets-cloud01.com'
-        );
+    $creditCardMethod->getRequest()->basketData(
+       '1234',
+       '15.30',
+       'EUR',
+        $configRepository->get('HeidelGatewayPlenty.secret')
+     );
 
-        $cardPaymentMethod->authorize(
-            'https://heidelpay-dev.plentymarkets-cloud01.com',
-            'TRUE',
-            null
-        );
-return $cardPaymentMethod->getResponse()->getPresentation()->getAmount();
+    $creditCardMethod->getRequest()->async(
+       'DE',
+       'https://heidelpay-dev.plentymarkets-cloud01.com'
+     );
+
+     $creditCardMethod->authorize(
+       'https://heidelpay-dev.plentymarkets-cloud01.com',
+       'TRUE',
+       null
+      );
+
+     return $creditCardMethod->getResponse()->getPresentation()->getAmount();
        /* if($cardPaymentMethod->getResponse()->isSuccess())
         {
            return $cardPaymentMethod->getRequest()->getPresentation()->getAmount();
