3fd708d12e7aeeb712525479ec69d836eca0bed6
Sascha Pflüger <sascha.pflueger@heidelpay.de>
2017-07-04 14:36:43 +0200

    test mit vorkasse 1.2

--- resources/lib/prepayment_request.php
+++ resources/lib/prepayment_request.php
@@ -4,11 +4,11 @@ $params = SdkRestApi::getParam("params");
 try {
     $creditCardMethod = new \Heidelpay\PhpApi\PaymentMethods\CreditCardPaymentMethod();
 
-    $creditCardMethod->getRequest()->authentification($params["authentification"]);
-    $creditCardMethod->getRequest()->customerAddress($params["customerAddress"]);
-    $creditCardMethod->getRequest()->basketData($params["basketData"]);
-    $creditCardMethod->getRequest()->async($params["async"]);
-    $creditCardMethod->authorize($params["authorize"]);
+    $creditCardMethod->getRequest()->authentification(...$params["authentification"]);
+    $creditCardMethod->getRequest()->customerAddress(...$params["customerAddress"]);
+    $creditCardMethod->getRequest()->basketData(...$params["basketData"]);
+    $creditCardMethod->getRequest()->async(...$params["async"]);
+    $creditCardMethod->authorize(...$params["authorize"]);
 
     return $creditCardMethod->getResponse()->getPresentation()->getAmount();
 
