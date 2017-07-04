8477565d5766cbe3076f45fe5a7c8451793dd60e
Sascha Pflüger <sascha.pflueger@heidelpay.de>
2017-07-04 15:23:10 +0200

    test mit vorkasse 1.9

--- resources/lib/prepayment_request.php
+++ resources/lib/prepayment_request.php
@@ -1,6 +1,6 @@
 <?php
 $params = SdkRestApi::getParam("params");
-$params = json_decode($params,true);
+//$params = json_decode($params,true);
 try {
     $creditCardMethod = new \Heidelpay\PhpApi\PaymentMethods\PrepaymentPaymentMethod();
 
